3d275ab15a9cc848c075ca2be4610d10aac6ffa1
Sanyam Kapoor <im@perhapsbay.es>
2021-06-01 10:15:24 +0530

    remove prose-sm mode

--- components/layout.js
+++ components/layout.js
@@ -172,7 +172,7 @@ const Layout = ({
       </Head>
       <Header />
       <div className="mx-auto px-4 py-5 max-w-3xl sm:px-6 sm:py-6 lg:max-w-4xl lg:py-8 lg:px-8 xl:max-w-6xl">
-        <div className="mx-auto prose prose-sm sm:prose lg:prose-lg xl:prose-xl">
+        <div className="mx-auto prose sm:prose lg:prose-lg xl:prose-xl 2xl:prose-2xl">
           {children}
         </div>
       </div>
