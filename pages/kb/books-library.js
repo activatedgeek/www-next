12838090a73337a6781584a16bd6ee9a1635db39
Sanyam Kapoor <im@sanyamkapoor.com>
2021-12-14 19:15:30 -0500

    update poster and book cover layouts

--- pages/kb/books-library.js
+++ pages/kb/books-library.js
@@ -3,44 +3,37 @@ import getAllBooks from "../../lib/librarything"
 import SinglePage from "../../components/single-page"
 import Rating from "../../components/rating"
 
-
 export default function BooksLibrary({ frontmatter, books }) {
   return (
     <SinglePage frontmatter={frontmatter}>
-      <p>
-        My library of books, fetched from my account at LibraryThing. Often some
-        titles may have an older publication date.
-      </p>
-      <table>
-        <tbody>
-          <tr>
-            <th>Year</th>
-            <th>Title / Rating</th>
-            <th>Author</th>
-          </tr>
-          {
-            books.map(({ isbn, year, cover, title, author, rating }) => (
-              <tr key={isbn}>
-                <td>{year}</td>
-                <td className="flex items-center">
-                  <img className="inline-block m-0" src={cover} alt={title} width={50} height={66} loading="lazy" />
-                  <div className="border-box pl-3">
-                  <a href={`https://www.librarything.com/search.php?search=${encodeURIComponent(title)}`} target="_blank" rel="noopener noreferrer">{title}</a>
-                  <Rating value={rating} />
-                  </div>
-                </td>
-                <td>
-                <i>{author}</i>
-                </td>
-              </tr>
-            ))
-          }
-        </tbody>
-      </table>
+      <p>My library of books, fetched from my account at LibraryThing.</p>
+      <div className="flex flex-row flex-wrap items-center">
+        {books.map(({ isbn, year, cover, title, rating }) => (
+          <div key={isbn} className="flex flex-col items-center text-sm">
+            <a
+              href={`https://www.librarything.com/search.php?search=${encodeURIComponent(
+                title
+              )}`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                className="inline-block m-3"
+                src={cover}
+                alt={title}
+                width={150}
+                height={300}
+                loading="lazy"
+              />
+            </a>
+            <Rating value={rating} />
+            <span>({year})</span>
+          </div>
+        ))}
+      </div>
     </SinglePage>
   )
 }
-
 
 export async function getStaticProps() {
   const { libraryThingUserId: userid } = getServerConfig()
@@ -49,13 +42,13 @@ export async function getStaticProps() {
   return {
     props: {
       frontmatter: {
-        title: 'My Books Library',
-        description: 'My collection fetched from LibraryThing',
-        area: 'cult',
-        cat: 'lit',
-        date: 'May 17 2021, 19:10 +0530',
+        title: "My Books Library",
+        description: "My collection fetched from LibraryThing",
+        area: "cult",
+        cat: "lit",
+        date: "May 17 2021, 19:10 +0530",
       },
-      books
+      books,
     },
   }
 }
--- pages/kb/movies-tv-library.js
+++ pages/kb/movies-tv-library.js
@@ -4,7 +4,6 @@ import { getServerConfig } from "../../lib/next-config"
 import SinglePage from "../../components/single-page"
 import { getFavoriteMovies, getFavoriteTv } from "../../lib/tmdb"
 
-
 export default function MoviesLibrary({ frontmatter, movies, tv }) {
   return (
     <SinglePage frontmatter={frontmatter}>
@@ -12,58 +11,64 @@ export default function MoviesLibrary({ frontmatter, movies, tv }) {
         <nav className="toc">
           <h3>Table of Contents</h3>
           <ul className="toc-level toc-level-1">
-            <li className="toc-item toc-item-h2"><a className="toc-link toc-link-h2" href="#movies">Movies</a></li>
-            <li className="toc-item toc-item-h2"><a className="toc-link toc-link-h2" href="#tv">TV</a></li>
+            <li className="toc-item toc-item-h2">
+              <a className="toc-link toc-link-h2" href="#movies">
+                Movies
+              </a>
+            </li>
+            <li className="toc-item toc-item-h2">
+              <a className="toc-link toc-link-h2" href="#tv">
+                TV
+              </a>
+            </li>
           </ul>
         </nav>
       </div>
-      <p>
-        My library of favorites, fetched from my account at TMDb.
-      </p>
+      <p>My library of favorites, fetched from my account at TMDb.</p>
       <h2 id="movies">Movies</h2>
-      <table>
-        <tbody>
-          <tr>
-            <th>Release Date</th>
-            <th>Title</th>
-          </tr>
-          {
-            movies.map(({ id, release_date, title, poster_path }) => (
-              <tr key={id}>
-                <td>{format(new Date(release_date), "yyyy")}</td>
-                <td className="inline-flex items-center">
-                  <a href={`https://www.themoviedb.org/movie/${id}`} target="_blank" rel="noopener noreferrer">
-                    <img className="inline m-0 mr-3" src={`https://www.themoviedb.org/t/p/w150_and_h225_bestv2${poster_path}`} alt={title} width={50} height={66} loading="lazy" />
-                    {title}
-                  </a>
-                </td>
-              </tr>              
-            ))
-          }
-        </tbody>
-      </table>
+      <div className="flex flex-row flex-wrap items-center">
+        {movies.map(({ id, release_date, title, poster_path }) => (
+          <div key={id} className="flex flex-col items-center text-sm">
+            <a
+              href={`https://www.themoviedb.org/movie/${id}`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                className="inline-block m-3"
+                src={`https://www.themoviedb.org/t/p/w150_and_h225_bestv2${poster_path}`}
+                alt={title}
+                width={100}
+                height={150}
+                loading="lazy"
+              />
+            </a>
+            <span>({format(new Date(release_date), "yyyy")})</span>
+          </div>
+        ))}
+      </div>
       <h2 id="tv">TV</h2>
-      <table>
-        <tbody>
-          <tr>
-            <th>First Aired</th>
-            <th>Title</th>
-          </tr>
-          {
-            tv.map(({ id, first_air_date, name, poster_path }) => (
-              <tr key={id}>
-                <td>{format(new Date(first_air_date), "yyyy")}</td>
-                <td className="inline-flex items-center">
-                  <img className="inline m-0 mr-3" src={`https://www.themoviedb.org/t/p/w150_and_h225_bestv2${poster_path}`} alt={name} width={50} height={66} loading="lazy" />
-                  <a href={`https://www.themoviedb.org/tv/${id}`} target="_blank" rel="noopener noreferrer">
-                    {name}
-                  </a>
-                </td>
-              </tr>
-            ))
-          }
-        </tbody>
-      </table>
+      <div className="flex flex-row flex-wrap items-center">
+        {tv.map(({ id, first_air_date, name, poster_path }) => (
+          <div key={id} className="flex flex-col items-center text-sm">
+            <a
+              href={`https://www.themoviedb.org/tv/${id}`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                className="inline m-3"
+                src={`https://www.themoviedb.org/t/p/w150_and_h225_bestv2${poster_path}`}
+                alt={name}
+                width={100}
+                height={150}
+                loading="lazy"
+              />
+            </a>
+            <span>({format(new Date(first_air_date), "yyyy")})</span>
+          </div>
+        ))}
+      </div>
     </SinglePage>
   )
 }
@@ -74,14 +79,14 @@ export async function getStaticProps() {
   return {
     props: {
       frontmatter: {
-        title: 'My Movies and TV Library',
-        description: 'My collection fetched from TMDb',
-        area: 'cult',
-        cat: 'arts',
-        date: 'Jul 24 2021, 13:50 +0530',
+        title: "My Movies and TV Library",
+        description: "My collection fetched from TMDb",
+        area: "cult",
+        cat: "arts",
+        date: "Jul 24 2021, 13:50 +0530",
       },
       movies: await getFavoriteMovies({ apiKey, sessionId }),
-      tv: await getFavoriteTv({ apiKey, sessionId })
+      tv: await getFavoriteTv({ apiKey, sessionId }),
     },
   }
 }
