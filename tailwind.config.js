db4fbb622fb58109c5d8671da1d0abb5f82f944d
Sanyam Kapoor <sanyam@nyu.edu>
2022-08-02 03:32:42 -0700

    update tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,17 +3,11 @@ const defaultTheme = require("tailwindcss/defaultTheme")
 module.exports = {
   // @NOTE https://github.com/tailwindlabs/tailwindcss-typography/issues/26.
   important: true,
-  purge: {
-    content: [
-      "./components/**/*.{js,ts,jsx,tsx}",
-      "./lib/**/*.{js,ts,jsx,tsx}",
-      "./pages/**/*.{js,ts,jsx,tsx}",
-    ],
-    options: {
-      whitelist: [],
-    },
-  },
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./lib/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       maxHeight: {
