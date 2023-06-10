293f975df77b979e8b8264b5158d2c8433d46c00
Sanyam Kapoor <sanyam@nyu.edu>
2023-06-09 21:33:54 -0400

    fix logo alignment

--- app/header.js
+++ app/header.js
@@ -8,18 +8,20 @@ import SignedInNav from "./signedInNav"
 export default function Header() {
   return (
     <div className="mx-auto z-10 opacity-95 flex justify-between bg-white top-0 py-3 px-3 max-w-3xl sm:px-6 lg:max-w-4xl lg:px-9 xl:max-w-6xl border-b-2">
-      <Link className="cursor-pointer" href="/">
-        <Image
-          src="/sk.svg"
-          alt="SK Logo"
-          width={70}
-          height={70}
-          className="shadow-none"
-        />
-      </Link>
+      <div className="flex items-center">
+        <Link className="cursor-pointer" href="/">
+          <Image
+            src="/sk.svg"
+            alt="SK Logo"
+            width={70}
+            height={70}
+            className="shadow-none"
+          />
+        </Link>
+      </div>
       <div className="flex items-center">
         <Link
-          className="mr-3 p-2 text-link rounded-md hover:bg-zinc-50"
+          className="ml-6 mr-3 p-2 text-link rounded-md hover:bg-zinc-50"
           href="/"
         >
           <FontAwesomeIcon icon={faHome} /> Home
