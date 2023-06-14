93343690425c467ed2f19cb4b0a83574d8e4305e
Sanyam Kapoor <sanyam@nyu.edu>
2023-06-14 16:01:43 -0400

    layout fix

--- components/areaLink.js
+++ components/areaLink.js
@@ -6,11 +6,10 @@ import { areas } from "@/api/cms"
 export default function AreaLink({ area }) {
   const { textColor, bgColor, ringColor, emoji_code } = areas[area]
   return (
-    <Link
-      className={`inline-flex items-center rounded-md px-2 py-1 text-sm ${textColor} hover:shadow-md ring-1 ring-inset ${bgColor} ${ringColor}`}
-      href={`/kb/overview/${area}`}
-    >
-      {emoji.get(emoji_code)} {area}
-    </Link>
+    <span className={`inline-flex items-center rounded-md px-2 py-1 text-sm hover:shadow-md ring-1 ring-inset ${bgColor} ${ringColor}`}>
+      <Link className={`${textColor}`} href={`/kb/overview/${area}`}>
+        {emoji.get(emoji_code)} {area}
+      </Link>
+    </span>
   )
 }
--- components/pageInfo.js
+++ components/pageInfo.js
@@ -13,20 +13,20 @@ export default function PageInfo({ title, description, date, updated, area }) {
     <>
       <h1 className="!mb-0">{title}</h1>
       <span className="text-gray-500">{description}</span>
-      <div className="flex flex-row flex-wrap items-center py-2 text-sm">
+      <div className="flex flex-row flex-wrap items-center text-sm">
         {date ? (
-          <span className="inline-block mr-3">
+          <span className="inline-flex flex-nowrap mr-2 my-2 items-center">
             <FontAwesomeIcon icon={faCalendarDay} className="mr-2" />
             <span className="rounded-md px-2 py-1 text-slate-500 ring-1 ring-inset bg-slate-100 ring-slate-500">{format(new Date(date), "MMM d, yyyy")}</span>
           </span>
         ) : null}
         {updated ? (
-          <span className="inline-block mr-3">
+          <span className="inline-flex flex-nowrap mr-2 my-2 items-center">
             <FontAwesomeIcon icon={faCalendarPlus} className="mr-2" />
             <span className="rounded-md px-2 py-1 text-slate-500 ring-1 ring-inset bg-slate-100 ring-slate-500">Last updated: {format(new Date(updated), "MMM d, yyyy")}</span>
           </span>
         ) : null}
-        <div className="inline-flex flex-nowrap items-center">
+        <div className="inline-flex flex-nowrap my-2 items-center">
           <FontAwesomeIcon icon={faTags} className="mr-2" />
           <AreaLink area={area} />
         </div>
