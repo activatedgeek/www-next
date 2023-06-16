3b97126efc3b192bc517e7a9b945da32ae6e39ad
Sanyam Kapoor <sanyam@nyu.edu>
2023-06-15 22:43:56 -0400

    fix redirects

--- app/[...notFound]/page.js
+++
@@ -1,4 +0,0 @@
-import NotFound from "../not-found"
-
-// @FIXME: https://github.com/vercel/next.js/issues/48763#issuecomment-1520385294
-export default NotFound
--- next.config.js
+++ next.config.js
@@ -1,3 +1,39 @@
+const fs = require("fs").promises
+const matter = require("gray-matter")
+
+async function getAllRedirects() {
+  const baseDir = process.env.WWW_KB_DIR
+  if (!baseDir) {
+    throw new Error("Missing WWW_KB_DIR environment variable.")
+  }
+
+  const { globby } = await import("globby")
+  const rawPaths = await globby([baseDir], {
+    expandDirectories: { extensions: ["md"] },
+  })
+
+  const { default: GithubSlugger } = await import("github-slugger")
+  const slugger = new GithubSlugger()
+  let allRedirects = await Promise.all(
+    rawPaths.map(async (filePath) => {
+      const rawString = await fs.readFile(filePath)
+      const {
+        data: { title, slug: _slug, redirectsFrom },
+      } = matter(rawString)
+      const slug = _slug ? _slug : slugger.slug(title)
+      if (redirectsFrom) {
+        return redirectsFrom.map((source) => ({
+          source,
+          destination: `/kb/${slug}`,
+          permanent: true,
+        }))
+      }
+    })
+  )
+
+  return allRedirects.filter((r) => r !== undefined).flat()
+}
+
 module.exports = {
   images: {
     domains: ["images-na.ssl-images-amazon.com", "pics.cdn.librarything.com"],
@@ -10,14 +46,9 @@ module.exports = {
     dirs: ["api", "app", "components"],
   },
   async redirects() {
-    let allRedirects = [
-      { source: "/kb/about", destination: "/", permanent: false },
+    return [
+      { source: "/kb/about", destination: "/", permanent: true },
+      ...(await getAllRedirects()),
     ]
-    // allPages.map(({ slug: destination, redirectsFrom }) => {
-    //   for (const source of redirectsFrom) {
-    //     allRedirects.push({ source, destination, permanent: false })
-    //   }
-    // })
-    return allRedirects
   },
 }
