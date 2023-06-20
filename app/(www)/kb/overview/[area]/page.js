19e48553172f56cc6878d97c4b7c3431e7b24be9
Sanyam Kapoor <sanyam@nyu.edu>
2023-06-19 22:32:06 -0400

    fix static overview page generation

--- app/(www)/kb/overview/[area]/page.js
+++ app/(www)/kb/overview/[area]/page.js
@@ -1,18 +1,22 @@
-import { getAllPublicPagesByArea } from "@/api/cms"
+import { getAllPublicPagesByArea, areas } from "@/api/cms"
 import PageList from "@/components/pageList"
 import Page, { generateMetadata as _generateMetadata } from "../../[slug]/page"
 
 export const dynamic = "force-static"
 
+export async function generateStaticParams() {
+  return Object.keys(areas).map((a) => ({ area: a }))
+}
+
 export async function generateMetadata({ params: { area } }) {
-  return await _generateMetadata({ params: { slug: `/kb/overview/${area}` } })
+  return await _generateMetadata({ params: { slug: `overview/${area}` } })
 }
 
 export default async function OverviewPage({ params: { area } }) {
   const areaPages = await getAllPublicPagesByArea(area)
   return (
     <>
-      {await Page({ params: { slug: `/kb/overview/${area}` } })}
+      {await Page({ params: { slug: `overview/${area}` } })}
       <PageList title="All Pages" pages={areaPages} />
     </>
   )
